af43d29a5c238b2e4ceaa72be599a84d92771a3c
Dmitry Burlakov <dburlakov@mtsbank.ru>
2023-03-28 13:08:48 +0300

    небольшие фиксы

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,21 @@
+import os.path
+
 import gdown
 import streamlit as st
 from catboost import CatBoostRegressor
 
-with st.spinner('Подготовка модели...'):
-    url = 'https://drive.google.com/uc?id=1wklHdPCrnsxyF9vb6XtdrcFAWC2qPZag'
-    output = 'used-cars-catboost.model'
-    gdown.download(url, output, quiet=False)
+st.title('Used car''s price prediction')
+
+with st.spinner('Подготовка модели...') as spinner:
+    if not os.path.exists('used-cars-catboost.model'):
+        url = 'https://drive.google.com/uc?id=1wklHdPCrnsxyF9vb6XtdrcFAWC2qPZag'
+        output = 'used-cars-catboost.model'
+        gdown.download(url, output, quiet=False)
 
     model = CatBoostRegressor()
     model.load_model('used-cars-catboost.model')
+    # st.success('Готово!')
 
-st.title('Used car''s price prediction')
 btn_features = st.button('Важность признаков')
 if btn_features:
     with st.spinner('Построение графика...'):
@@ -18,4 +23,4 @@ if btn_features:
         st.write(features)
         ax = features.plot.barh(x='Feature Id', y='Importances')
         st.pyplot(ax.figure)
-
+    # st.success('Готово!')
