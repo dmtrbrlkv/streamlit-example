63947c3c213b4dda9af7d08aff0e57f948572b3b
Dmitry Burlakov <dburlakov@mtsbank.ru>
2023-03-28 12:57:50 +0300

    спиннеры

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,17 +3,20 @@ import py7zr
 import streamlit as st
 from catboost import CatBoostRegressor
 
-url = 'https://drive.google.com/uc?id=1wklHdPCrnsxyF9vb6XtdrcFAWC2qPZag'
-output = 'used-cars-catboost.model'
-gdown.download(url, output, quiet=False)
+with st.spinner('Подготовка модели...'):
+    url = 'https://drive.google.com/uc?id=1wklHdPCrnsxyF9vb6XtdrcFAWC2qPZag'
+    output = 'used-cars-catboost.model'
+    gdown.download(url, output, quiet=False)
 
-model = CatBoostRegressor()
-model.load_model('used-cars-catboost.model')
+    model = CatBoostRegressor()
+    model.load_model('used-cars-catboost.model')
 
+st.title('Used car''s price prediction')
 btn_features = st.button('Важность признаков')
 if btn_features:
-    features = model.get_feature_importance(prettified=True).sort_values('Importances')
-    st.title('Used car''s price prediction')
-    st.write(features)
-    ax = features.plot.barh(x='Feature Id', y='Importances')
-    st.pyplot(ax.figure)
+    with st.spinner('Построение графика...'):
+        features = model.get_feature_importance(prettified=True).sort_values('Importances')
+        st.write(features)
+        ax = features.plot.barh(x='Feature Id', y='Importances')
+        st.pyplot(ax.figure)
+
