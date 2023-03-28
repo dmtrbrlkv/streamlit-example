9364587a6aae6a5b344fb867139398c9d80db13b
Dmitry Burlakov <dburlakov@mtsbank.ru>
2023-03-28 13:46:00 +0300

    кэш загрухки модели

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,15 +6,22 @@ from catboost import CatBoostRegressor
 
 st.title('Used car''s price prediction')
 
-with st.spinner('Подготовка модели...') as spinner:
-    if not os.path.exists('used-cars-catboost.model'):
-        url = 'https://drive.google.com/uc?id=1wklHdPCrnsxyF9vb6XtdrcFAWC2qPZag'
-        output = 'used-cars-catboost.model'
-        gdown.download(url, output, quiet=False)
 
-    model = CatBoostRegressor()
-    model.load_model('used-cars-catboost.model')
-    # st.success('Готово!')
+@st.cache(ttl=24*3600)
+def load_model():
+    with st.spinner('Подготовка модели...') as spinner:
+        if not os.path.exists('used-cars-catboost.model'):
+            url = 'https://drive.google.com/uc?id=1wklHdPCrnsxyF9vb6XtdrcFAWC2qPZag'
+            output = 'used-cars-catboost.model'
+            gdown.download(url, output, quiet=False)
+
+        model = CatBoostRegressor()
+        model.load_model('used-cars-catboost.model')
+        return model
+        # st.success('Готово!')
+
+
+model = load_model()
 
 btn_features = st.button('Важность признаков')
 if btn_features:
