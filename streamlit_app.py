a33d120db4a2c7de42f1cbfd8bb48fa116974a19
Dmitry Burlakov <dburlakov@mtsbank.ru>
2023-03-28 12:47:14 +0300

    Признаки по кнопке

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,8 +13,11 @@ archive.close()
 
 model = CatBoostRegressor()
 model.load_model('used-cars-catboost.model')
-features = model.get_feature_importance(prettified=True).sort_values('Importances')
-st.title('Used car''s price prediction')
-st.write(features)
-ax = features.plot.barh(x='Feature Id', y='Importances')
-st.pyplot(ax.figure)
+
+btn_features = st.button('Важность признаков')
+if btn_features:
+    features = model.get_feature_importance(prettified=True).sort_values('Importances')
+    st.title('Used car''s price prediction')
+    st.write(features)
+    ax = features.plot.barh(x='Feature Id', y='Importances')
+    st.pyplot(ax.figure)
