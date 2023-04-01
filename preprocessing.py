c7ff4f6c052693b1b9416f05c74a540bd407abb4
Dmitry Burlakov <dburlakov@mtsbank.ru>
2023-04-01 13:21:17 +0300

    Добавил предсказание стоимости

---
+++ preprocessing.py
@@ -0,0 +1,74 @@
+import pandas as pd
+
+
+def preprocessing(df, features):
+    UNKNOWN = 'UNKNOWN'
+
+    categorical_features = ['make', 'model', 'trim', 'body', 'state', 'color', 'interior', 'seller']
+
+    for column in categorical_features:
+        df[column] = df[column].replace({'': pd.NA})
+
+    for column in categorical_features:
+        df[column] = df[column].str.lower()
+
+    df[categorical_features] = df[categorical_features].fillna(UNKNOWN)
+
+    df['saledate_dt'] = pd.to_datetime(df['saledate'], utc=True)
+    df['sale_year'] = df['saledate_dt'].dt.year
+    df['sale_month'] = df['saledate_dt'].dt.month
+    df["sale_yearday"] = df["saledate_dt"].dt.dayofyear
+    df["sale_weekday"] = df["saledate_dt"].dt.dayofweek
+    df["sale_day"] = df["saledate_dt"].dt.day
+
+    df['trim'] = df['trim'].replace({UNKNOWN.lower(): 'base'})
+
+    body_replace = {
+        'cts coupe': 'coupe',
+        'cts wagon': 'wagon',
+        'cts-v coupe': 'coupe',
+        'cts-v wagon': 'wagon',
+        'e-series van': 'van',
+        'g convertible': 'convertible',
+        'g coupe': 'coupe',
+        'g sedan': 'sedan',
+        'g37 convertible': 'convertible',
+        'g37 coupe': 'coupe',
+        'genesis coupe': 'coupe',
+        'granturismo convertible': 'convertible',
+        'koup': 'coupe',
+        'promaster cargo van': 'van',
+        'q60 convertible': 'convertible',
+        'q60 coupe': 'coupe',
+        'regular-cab': 'regular cab',
+        'transit van': 'van',
+        'tsx sport wagon': 'wagon',
+        'ram van': 'van',
+        'beetle convertible': 'convertible'
+    }
+    df['body'] = df['body'].replace(body_replace)
+
+    df['color'] = df['color'].replace({'—': UNKNOWN})
+
+    df['interior'] = df['interior'].replace({'—': UNKNOWN})
+
+    df['seller'] = df['seller'].str.replace('-', ' ', regex=False)
+    df['seller'] = df['seller'].str.replace('/', ' ', regex=False)
+    df['seller'] = df['seller'].str.replace(',', ' ', regex=False)
+    df['seller'] = df['seller'].str.replace('.', ' ', regex=False)
+    df['seller'] = df['seller'].str.replace('(', ' ', regex=False)
+    df['seller'] = df['seller'].str.replace(')', ' ', regex=False)
+    df['seller'] = df['seller'].str.replace('   ', ' ', regex=False)
+    df['seller'] = df['seller'].str.replace('  ', ' ', regex=False)
+
+    df['condition_div_by_odometer'] = df['condition'] / df['odometer']
+    df['age'] = df['sale_year'] - df['year']
+    df['condition_div_by_age'] = df['condition'] / df['age']
+    df['odometer_div_by_age'] = df['odometer'] / df['age']
+    df['make_model'] = df['make'] + df['model']
+    df['model_trim'] = df['model'] + df['trim']
+    df['model_body'] = df['model'] + df['body']
+    df['model_trim_body'] = df['model'] + df['trim'] + df['body']
+    df['make_model_trim_body'] = df['make'] + df['model'] + df['trim'] + df['body']
+
+    return df[features]
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,12 @@
 import os.path
 
 import gdown
+import pandas
 import streamlit as st
 from catboost import CatBoostRegressor
+from dateutil.utils import today
+
+from preprocessing import preprocessing
 
 st.title('Used car''s price prediction')
 
@@ -20,16 +24,13 @@ def load_model():
     # st.success('Готово!')
 
 
-
-
-
 with st.spinner('Подготовка модели...') :
-    model = load_model()
+    model_cb = load_model()
 
 
 @st.cache_data
 def features_graph():
-    features = model.get_feature_importance(prettified=True)
+    features = model_cb.get_feature_importance(prettified=True)
     st.write(features)
     features = features.sort_values('Importances')
     ax = features.plot.barh(x='Feature Id', y='Importances')
@@ -37,8 +38,50 @@ def features_graph():
 
 
 btn_features = st.button('Важность признаков')
+
 if btn_features:
     with st.spinner('Построение графика...'):
         fig = features_graph()
         st.pyplot(fig)
     # st.success('Готово!')
+
+# btn_predict_form = st.button('Предсказать цену')
+# if btn_predict_form:
+year = st.number_input('Year', min_value=0, max_value=today().year)
+make = st.text_input('Make')
+model = st.text_input('Model')
+trim = st.text_input('Trim')
+body = st.text_input('Body')
+transmission = st.radio('Transmission', ['automatic', 'manual'])
+state = st.text_input('State', max_chars=2)
+condition = st.number_input('Condition', min_value=1.0, max_value=5.0, step=0.5)
+odometer = st.number_input('Odometer', min_value=0)
+color = st.text_input('Color')
+interior = st.text_input('Interior')
+seller = st.text_input('Seller')
+saledate = st.date_input('Sale date', today())
+
+btn_predict = st.button('Предсказать')
+
+if btn_predict:
+    df = pandas.DataFrame({
+        'year': [year],
+        'make': [make],
+        'model': [model],
+        'trim': [trim],
+        'body': [body],
+        'transmission': [transmission],
+        'state': [state],
+        'condition': [condition],
+        'odometer': [odometer],
+        'color': [color],
+        'interior': [interior],
+        'seller': [seller],
+        'saledate': [saledate],
+    })
+
+    df = preprocessing(df, model_cb.feature_names_)
+
+    predict = model_cb.predict(df)
+
+    st.write(f'Цена: {predict[0]:.2f}')
