6798f3227d2d71cde440012d204360bf23d7d6ed
Dmitry Burlakov <dburlakov@mtsbank.ru>
2023-04-02 21:31:25 +0300

    Обновил модель на облегченную

--- load_model.py
+++ load_model.py
@@ -5,10 +5,10 @@ import streamlit as st
 from catboost import CatBoostRegressor
 
 
-@st.cache_resource()
+@st.cache_resource(show_spinner=False)
 def load_model():
     if not os.path.exists('used-cars.cdm'):
-        url = 'https://drive.google.com/uc?id=13VwUzi_NrOwFlBjwB8p3tX8XQE9P34ZP'
+        url = 'https://drive.google.com/uc?id=1_yVEvHEF-m-H-bem_N37-swObJWIyMyk'
         output = 'used-cars.cdm'
         gdown.download(url, output, quiet=False)
 
